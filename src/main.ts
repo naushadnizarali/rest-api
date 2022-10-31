9453ab99fb2fd6d30a6f9b4f15f0a3e8ecaf3f59
Naushad Nizar Ali <naushadnizar@outlook.com>
2022-10-31 16:34:08 +0500

    feat: process.env

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 6661;
+  const port = process.env.PORT || 6661;
   await app.listen(port, () => {
     Logger.log(`Listening at http://localhost:${port}`);
   });
